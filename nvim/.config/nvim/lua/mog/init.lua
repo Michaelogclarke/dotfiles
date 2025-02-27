98fc2ae94801ae246131c38b335ca3df8bcdef9a
michaelogclarke <michaelogclarke@gmail.com>
2025-02-27 14:18:44 +0000

    added sway stuff

--- nvim/.config/nvim/lua/mog/init.lua
+++ nvim/.config/nvim/lua/mog/init.lua
@@ -1,2 +1,8 @@
 print("smells")
 require("mog.remap")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
